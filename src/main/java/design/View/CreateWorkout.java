818755159653079b47360dde643b1626bf876348
V-Karch <luna.karch05@gmail.com>
2025-02-20 21:04:34 -0500

    View/CreateWorkout: Added note with questions

--- src/main/java/design/View/CreateWorkout.java
+++ src/main/java/design/View/CreateWorkout.java
@@ -9,6 +9,13 @@ public class CreateWorkout implements Action {
         this.workoutBuilder = workoutBuilder;
     }
 
+    // NOTE: I am not sure if this file is how this command is intended to work
+    // I also made the change and assumption of a <<create>> for these Commands since they
+    // are not shown in the draw.io
+    // I also noticed that on the WorkoutCreation draw.io page
+    // There is a Workout().addDailyCalories() method which is now shown on the Workout page
+    // Is this intentional?
+
     @Override
     public void execute() {
         this.workoutBuilder.createWorkout();
