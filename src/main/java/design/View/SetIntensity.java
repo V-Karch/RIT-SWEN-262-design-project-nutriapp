20cddb7428c3eeafa3a4f76796b5da1f0c3af8bb
V-Karch <luna.karch05@gmail.com>
2025-02-20 20:55:57 -0500

    changed name of ChangeIntensity to SetIntensity

--- src/main/java/design/View/SetIntensity.java
+++ src/main/java/design/View/SetIntensity.java
@@ -3,12 +3,12 @@ package design.View;
 import design.Model.Intensity;
 import design.Controller.WorkoutBuilder;
 
-public class ChangeIntensity implements Action {
+public class SetIntensity implements Action {
     
     private Intensity intensity;
     private WorkoutBuilder workoutBuilder;
 
-    public ChangeIntensity(WorkoutBuilder workoutBuilder, Intensity intensity) {
+    public SetIntensity(WorkoutBuilder workoutBuilder, Intensity intensity) {
         this.workoutBuilder = workoutBuilder;
         this.intensity = intensity;
     }
