ab6cea1af1cc20f604d06a9698f40f93b27239f1
V-Karch <luna.karch05@gmail.com>
2025-02-20 20:39:11 -0500

    Controller/WorkoutBuilder: Added clarified version of WorkoutBuilder

---
+++ src/main/java/design/Controller/WorkoutBuilder.java
@@ -0,0 +1,36 @@
+package design.Controller;
+
+import design.Model.Workout;
+import design.Model.Intensity;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class WorkoutBuilder {
+    private String date;
+    private Intensity intensity;
+    private int minutes;
+
+    public WorkoutBuilder() {
+        this.date = "Unset";
+        this.intensity = Intensity.MEDIUM;
+        this.minutes = 0;
+    }
+
+    public void setMinutes(int minutes) {
+        this.minutes = minutes;
+    }
+
+    public void setIntensity(Intensity intensity) {
+        this.intensity = intensity;
+    }
+
+    public void setDateTime() {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        this.date = now.format(formatter);
+    }
+
+    public Workout createWorkout() {
+        return new Workout(minutes, intensity, date);
+    }
+}
