642ae3f23be4e3686c3e7a0afd069043b07b153e
V-Karch <luna.karch05@gmail.com>
2025-02-20 20:35:33 -0500

    Controller/WorkoutManager: Reimplemented with hashmap

--- src/main/java/design/Controller/WorkoutManager.java
+++ src/main/java/design/Controller/WorkoutManager.java
@@ -1,21 +1,34 @@
 package design.Controller;
 
-import java.util.List;
-import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
 import design.Model.Workout;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class WorkoutManager {
-    private List<Workout> workouts;
-    
+    private Map<String, Workout> workouts;
+
     public WorkoutManager() {
-        this.workouts = new ArrayList<>();
+        this.workouts = new HashMap<>();
+    }
+
+    private String getCurrentTime() {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        return now.format(formatter);
     }
 
     public void addWorkout(Workout workout) {
-        workouts.add(workout);
+        String currentTime = this.getCurrentTime();
+        this.workouts.put(currentTime, workout);
     }
 
-    public List<Workout> getWorkouts() {
+    public Workout getWorkout(String date) {
+        return this.workouts.get(date);
+    }
+
+    public Map<String, Workout> getWorkouts() {
         return this.workouts;
     }
 }
