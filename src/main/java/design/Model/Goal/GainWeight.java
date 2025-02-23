2275eb87cdb0597917eb86d96b8b80a465054964
Anthony Guo <ayg9231@rit.edu>
2025-02-23 13:50:08 -0500

    gain weight base

--- src/main/java/design/Model/Goal/GainWeight.java
+++ src/main/java/design/Model/Goal/GainWeight.java
@@ -1,5 +1,37 @@
 package design.Model.Goal;
 
 public class GainWeight implements Goal{
-  
+  private User user;
+  private boolean physicalFitness;
+  private int targetCalories;
+  private int dailyCalories;
+
+  @Override
+  public int getTargetCalories() {
+    return targetCalories;
+  }
+
+  @Override
+  public int removeDailyCalories(int calories) {
+    dailyCalories -= calories;
+    return dailyCalories;
+  }
+
+  @Override
+  public int addDailyCalories(int calories) {
+    dailyCalories += calories;
+    return dailyCalories;
+  }
+
+  @Override
+  public void handleWeightChange() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'handleWeightChange'");
+  }
+
+  @Override
+  public void handleTargetWeightChange() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'handleTargetWeightChange'");
+  }
 }
