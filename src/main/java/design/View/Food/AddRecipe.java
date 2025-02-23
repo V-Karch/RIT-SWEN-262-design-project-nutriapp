8569f984c8bbe6a764e3cf802d072aa41809e362
ChaseCollins83 <cmc1639@rit.edu>
2025-02-23 17:44:26 -0500

    updated AddRecipe in View

--- src/main/java/design/View/Food/AddRecipe.java
+++ src/main/java/design/View/Food/AddRecipe.java
@@ -1,13 +1,15 @@
 package design.View.Food;
 
 import design.Controller.FoodManager;
+import design.Model.Recipe;
 
 public class AddRecipe implements Action {
     private FoodManager foodManager;
     private Recipe recipe;
+    private Meal meal;
 
-    public AddRecipe(Recipe recipe,)
+    public AddRecipe(Recipe recipe, Meal meal)
     {
-
+        foodManager.addRecipe(meal, recipe);
     }
 }
