f1a989180cc5f9d1fb99d63cfd215887c0f8f3ad
Anthony Guo <ayg9231@rit.edu>
2025-02-24 21:23:06 -0500

    set goal command

---
+++ src/main/java/design/View/Goal/SetGoal.java
@@ -0,0 +1,23 @@
+package design.View.Goal;
+
+import java.util.Scanner;
+
+import design.Controller.Goal.GoalManager;
+import design.Model.Goal.Goal;
+import design.View.Action;
+
+public class SetGoal implements Action {
+  private GoalManager goalManager;
+  private Goal goal;
+  
+  public SetGoal(GoalManager goalManager, Goal goal) {
+    this.goalManager = goalManager;
+    this.goal = goal;
+  }
+
+  @Override
+  public void execute() {
+    this.goalManager.setGoal(goal);
+  }
+  
+}
