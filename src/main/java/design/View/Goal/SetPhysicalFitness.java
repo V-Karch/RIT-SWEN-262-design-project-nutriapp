7f66beae1013e8efbd63ca7a007f6ae28dc729cc
Anthony Guo <ayg9231@rit.edu>
2025-02-25 20:09:37 -0500

    getter concrete commands

--- src/main/java/design/View/Goal/SetPhysicalFitness.java
+++ src/main/java/design/View/Goal/SetPhysicalFitness.java
@@ -5,28 +5,25 @@ import java.util.Scanner;
 import design.Controller.Goal.GoalManager;
 import design.View.Action;
 
-public class SetPhysicalFitness implements Action{
-    private GoalManager goalManager;
-    private Scanner scanner;
+public class SetPhysicalFitness implements Action {
+  private GoalManager goalManager;
+  private Scanner scanner;
 
-    public SetPhysicalFitness(GoalManager goalManager, Scanner scanner) {
-        this.goalManager = goalManager;
-        this.scanner = scanner;
-      }
+  public SetPhysicalFitness(GoalManager goalManager, Scanner scanner) {
+    this.goalManager = goalManager;
+    this.scanner = scanner;
+  }
 
-    @Override
-    public void execute() {
-        System.out.println("Would you like to improve physical fitness? (Y/N)");
-        
-        String response_Case = scanner.nextLine();
-        String response = response_Case.toLowerCase();
-        System.out.println(response);
-        if (response.equals("y")){
-            this.goalManager.setPhysicalFitness();
-            System.out.println("Lets get physical!");
-        } else {
-            System.out.println("The gym is overrated tbh");
-        }
+  @Override
+  public void execute() {
+    System.out.println("Enter target weight:");
+    try {
+      boolean doesFitness = scanner.nextBoolean();
+      this.goalManager.setPhysicalFitness(doesFitness);
+      System.out.println("Physical fitness updated.");
     }
-    
+    catch(Exception e) {
+      System.out.println("Invalid input, true or false required");
+    }
+  }
 }
