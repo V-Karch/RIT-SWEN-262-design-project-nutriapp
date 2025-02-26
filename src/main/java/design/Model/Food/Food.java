edfe0cc8f64b073cf2de0d9239f011644da0f94f
Malachi Levinson <mll8777@rit.edu>
2025-02-25 20:27:26 -0500

    Changed variable types to prevent number format exception.

--- src/main/java/design/Model/Food/Food.java
+++ src/main/java/design/Model/Food/Food.java
@@ -3,8 +3,8 @@ package design.Model.Food;
 public interface Food {
     public Integer getCalories();
     public String getName();
-    public Integer getFat();
-    public Integer getProtein();
-    public Integer getFiber();
-    public Integer getCarbs();
+    public Double getFat();
+    public Double getProtein();
+    public Double getFiber();
+    public Double getCarbs();
 }
