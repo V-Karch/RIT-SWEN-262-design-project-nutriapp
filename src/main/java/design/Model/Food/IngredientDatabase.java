a6f12d0fbee0faff7725b6c4fdf1aa6d11eec699
Liliana Cantillo <lncantillo@gmail.com~>
2025-02-26 20:27:26 -0500

    added functions for malachi

--- src/main/java/design/Model/Food/IngredientDatabase.java
+++ src/main/java/design/Model/Food/IngredientDatabase.java
@@ -2,28 +2,27 @@ package design.Model.Food;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import au.com.bytecode.opencsv.CSVReader;
-import java.util.ArrayList;
-
 
 public class IngredientDatabase {
-    private List<Ingredient> ingredients;
-    
 
-    public IngredientDatabase(String file) throws IOException{
+    private List<Ingredient> ingredients;
+
+    public IngredientDatabase(String file) throws IOException {
         FileReader reader = new FileReader(file);
-        CSVReader csvreader = new CSVReader(reader,',','"',1);
+        CSVReader csvreader = new CSVReader(reader, ',', '"', 1);
         this.ingredients = new ArrayList<Ingredient>();
         List<String[]> ingredientData = csvreader.readAll();
-        for (String[] ingredient : ingredientData){
-            try{
-            this.ingredients.add(new Ingredient(ingredient[1], Integer.parseInt(ingredient[3]), Double.parseDouble(ingredient[4]), Double.parseDouble(ingredient[5]), Double.parseDouble(ingredient[7]), Double.parseDouble(ingredient[8])));
-        } finally{
-            this.ingredients.add(new Ingredient(ingredient[1], Integer.parseInt(ingredient[3]), Double.parseDouble(ingredient[4]), Double.parseDouble(ingredient[5]), Double.parseDouble(ingredient[7])));
+        for (String[] ingredient : ingredientData) {
+            try {
+                this.ingredients.add(new Ingredient(ingredient[1], Integer.parseInt(ingredient[3]), Double.parseDouble(ingredient[4]), Double.parseDouble(ingredient[5]), Double.parseDouble(ingredient[7]), Double.parseDouble(ingredient[8])));
+            } finally {
+                this.ingredients.add(new Ingredient(ingredient[1], Integer.parseInt(ingredient[3]), Double.parseDouble(ingredient[4]), Double.parseDouble(ingredient[5]), Double.parseDouble(ingredient[7])));
+            }
         }
-    }
         csvreader.close();
     }
 
@@ -31,13 +30,29 @@ public class IngredientDatabase {
         return ingredients;
     }
 
-    public Ingredient getIngredient(String name){
-        for(Ingredient ingredient : ingredients){
-            if(ingredient.getName().equals(name)){
+    private int ensureInt(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
+
+    private double ensureDouble(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
+
+    public Ingredient getIngredient(String name) {
+        for (Ingredient ingredient : ingredients) {
+            if (ingredient.getName().equals(name)) {
                 return ingredient;
             }
         }
         return new Ingredient();
     }
-    
+
 }
