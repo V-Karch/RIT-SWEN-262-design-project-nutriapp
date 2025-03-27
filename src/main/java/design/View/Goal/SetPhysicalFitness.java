fbcc6a65f73203a9a966bcbbb92d4e34444433c3
Anthony Guo <ayg9231@rit.edu>
2025-03-27 00:06:25 -0400

    needs fixing

--- src/main/java/design/View/Goal/SetPhysicalFitness.java
+++ src/main/java/design/View/Goal/SetPhysicalFitness.java
@@ -1,31 +1,37 @@
 package design.View.Goal;
 
-import java.util.Scanner;
-
 import design.Controller.Goal.GoalManager;
 import design.View.Action;
+import design.View.NALogger;
 
 public class SetPhysicalFitness implements Action {
-  private GoalManager goalManager;
-  private Scanner scanner;
+    private GoalManager goalManager;
+    private NALogger logger;
 
-  public SetPhysicalFitness(GoalManager goalManager, Scanner scanner) {
-    this.goalManager = goalManager;
-    this.scanner = scanner;
-  }
+    public SetPhysicalFitness(GoalManager goalManager, NALogger logger) {
+        this.logger = logger;
+    }
 
-  @Override
-  public void execute() {
-    System.out.println("Would you like to include physical fitness? (true or false)");
-    System.out.print("$ ");
-    try {
-      String doesFitness_S = scanner.nextLine();
-      Boolean doesFitness = Boolean.getBoolean(doesFitness_S);
-      this.goalManager.setPhysicalFitness(doesFitness);
-      System.out.println("Physical fitness updated.");
+    @Override
+    public void execute() {
+        logger.print("Would you like to include physical fitness? (true or false)");
+        logger.print("\n$ ");
+        boolean validInput = false;
+        while (!validInput) {
+            try {
+                String bool = logger.readString();
+                //parseBoolean ALWAYS returns a boolean regardless of input, so checks for true or false before using it
+                if (!bool.equalsIgnoreCase("true") && !bool.equalsIgnoreCase("false")){
+                    throw new Exception();
+                }
+                validInput = true;
+                boolean doesFitness = Boolean.parseBoolean(bool);
+                System.out.println(doesFitness);
+                this.goalManager.setPhysicalFitness(doesFitness); //issue is here, maybe goal manager doesn't exist?
+                logger.print("Physical fitness updated.\n");
+            } catch(Exception e) {
+                logger.error("Invalid input, please enter true or false");
+            }
+        }
     }
-    catch(Exception e) {
-      System.out.println("Invalid input, true or false required");
-    }
-  }
 }
