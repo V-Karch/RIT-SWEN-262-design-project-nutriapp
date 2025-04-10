3edf41922690b9d588fa3342199f0ba7dea80887
Anthony Guo <ayg9231@rit.edu>
2025-04-10 13:00:24 -0400

    food stuff

--- src/test/java/Undo/FoodUndoTest.java
+++ src/test/java/Undo/FoodUndoTest.java
@@ -1,15 +1,25 @@
 package Undo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import java.lang.reflect.Array;
 
 import org.junit.jupiter.api.Test;
 
 import design.Controller.Food.FoodManager;
 import design.Controller.Goal.GoalManager;
+import design.Controller.History.HistoryController;
+import design.Controller.Undo.DailyActivityUndo;
 import design.Controller.Undo.FoodUndo;
+import design.Controller.Undo.GoalUndo;
 import design.Model.Food.Ingredient;
+import design.Model.Food.Meal;
+import design.Model.Food.Recipe;
 import design.Model.History.DailyActivity;
+import design.Model.Undo.DailyActivitySaveHistory;
 import design.Model.Undo.FoodSaveHistory;
+import design.Model.Undo.GoalSaveHistory;
 import design.Model.UserSS.User;
 
 public class FoodUndoTest {
@@ -30,6 +40,7 @@ public class FoodUndoTest {
 
         } catch(Exception e) {
             System.out.println("test failed");
+            assertEquals(0, 1);
         }
     }
 
@@ -40,15 +51,62 @@ public class FoodUndoTest {
             FoodSaveHistory foodSaveHistory = new FoodSaveHistory(foodManager);
             FoodUndo foodUndo = new FoodUndo(foodSaveHistory);
             foodUndo.storeSave();
-            Ingredient ingredient = foodManager.getIngredient("TURTLE GREEN RAW");
+            Ingredient ingredient = foodManager.getIngredient("TURTLE,GREEN,RAW");
             foodManager.updateStock(ingredient, 100);
             assertEquals(100, ingredient.getStock());
             foodUndo.restoreSave();
-            int actual = ingredient.getStock();
+            int actual = foodManager.getIngredient("TURTLE,GREEN,RAW").getStock();
             assertEquals(0, actual);
 
         } catch(Exception e) {
             System.out.println("test failed");
+            assertEquals(0, 1);
+        }
+    }
+
+    @Test
+    void testEverything() {
+        try {
+            //Food setup
+            FoodManager foodManager = new FoodManager("src/main/java/design/ingredients.csv");
+            FoodSaveHistory foodSaveHistory = new FoodSaveHistory(foodManager);
+            FoodUndo foodUndo = new FoodUndo(foodSaveHistory);
+            //store ingredient
+            Ingredient ingredient = foodManager.getIngredient("TURTLE,GREEN,RAW");
+            foodManager.updateStock(ingredient, 100);
+            //assertEquals(100, ingredient.getStock());
+            //make meal
+            String[] instructions = {"green", "raw"};
+            Recipe recipe = foodManager.createRecipe("turtle", instructions);
+            foodManager.addIngredient(recipe, ingredient.getName(), 100);
+            Meal meal = foodManager.createMeal("turtle green raw");
+            foodManager.addRecipe(meal, recipe);
+            foodUndo.storeSave();
+            DailyActivitySaveHistory dAHistory = new DailyActivitySaveHistory(dA);
+            DailyActivityUndo dActivityUndo = new DailyActivityUndo(dAHistory);
+            dActivityUndo.storeSave();
+            String expectedActivity = dA.logDayActivities();
+
+            GoalSaveHistory goalSaveHistory = new GoalSaveHistory(user);
+            GoalUndo goalUndo = new GoalUndo(goalSaveHistory);
+            goalUndo.storeSave();
+
+            //eat meal
+            foodManager.updateStock(ingredient, 100);
+            foodManager.prepareMeal(0, user.getGoal(), null);
+            
+
+            foodUndo.restoreSave();
+            goalUndo.restoreSave();
+            dActivityUndo.restoreSave();
+            int actual = ingredient.getStock();
+            assertEquals(0, actual);
+            assertEquals(0, goalManager.getDailyCalories());
+            assertEquals(expectedActivity, dA.logDayActivities());
+
+        } catch(Exception e) {
+            System.out.println("test failed");
+            assertEquals(0, 1);
         }
     }
 }
